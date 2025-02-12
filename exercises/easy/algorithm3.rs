0cf335e8c30ad282a38246b03be93190f63ed673
TakayaTsukima_ <85539511+AlanKSorata@users.noreply.github.com>
2025-02-12 09:55:58 +0000

    update

--- exercises/easy/algorithm3.rs
+++ exercises/easy/algorithm3.rs
@@ -1,13 +1,25 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
 
-
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T])
+where
+    T: Ord + Copy,
+{
+    //TODO
+    for i in 0..array.len() {
+        for j in 0..array.len() - i - 1 {
+            if array[j] > array[j + 1] {
+                let mut t = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = t;
+            }
+        }
+    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,13 +30,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
--- exercises/easy/algorithm5.rs
+++ exercises/easy/algorithm5.rs
@@ -1,14 +1,13 @@
 /*
-	bfs
-	This problem requires you to implement a basic BFS algorithm
+    bfs
+    This problem requires you to implement a basic BFS algorithm
 */
-
 
 use std::collections::VecDeque;
 
 // Define a graph
 struct Graph {
-    adj: Vec<Vec<usize>>, 
+    adj: Vec<Vec<usize>>,
 }
 
 impl Graph {
@@ -21,20 +20,56 @@ impl Graph {
 
     // Add an edge to the graph
     fn add_edge(&mut self, src: usize, dest: usize) {
-        self.adj[src].push(dest); 
-        self.adj[dest].push(src); 
+        self.adj[src].push(dest);
+        self.adj[dest].push(src);
     }
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
+        // TODO: 实现广度优先搜索（BFS）并返回访问顺序
 
+        // 用于存储访问顺序的向量
         let mut visit_order = vec![];
+
+        // 用于存储待访问节点的队列
+        let mut wait_vec = vec![];
+
+        // 将起始节点加入访问顺序和待访问队列
+        visit_order.push(start);
+        wait_vec.push(start);
+
+        // 临时变量，用于存储当前正在处理的节点
+        let mut temp = start;
+
+        // 当待访问队列不为空时，继续处理
+        while !wait_vec.is_empty() {
+            // 遍历当前节点的所有邻接节点
+            for i in 0..self.adj[temp].len() {
+                // 如果邻接节点未被访问过
+                if !visit_order.contains(&self.adj[temp][i]) {
+                    // 将邻接节点加入访问顺序
+                    visit_order.push(self.adj[temp][i]);
+
+                    // 如果邻接节点不在待访问队列中，则加入队列
+                    if !wait_vec.contains(&self.adj[temp][i]) {
+                        wait_vec.push(self.adj[temp][i]);
+                    }
+                }
+            }
+
+            // 移除队列中的第一个节点（当前节点）
+            wait_vec.remove(0);
+
+            // 如果队列不为空，更新临时变量为队列中的第一个节点
+            if !wait_vec.is_empty() {
+                temp = wait_vec[0];
+            }
+        }
+
+        // 返回访问顺序
         visit_order
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -84,4 +119,3 @@ mod tests {
         assert_eq!(visited_order, vec![0]);
     }
 }
-
