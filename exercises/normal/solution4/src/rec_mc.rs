a9112ea097a3e833abffa084b5b790ae9df68bb3
TakayaTsukima_ <85539511+AlanKSorata@users.noreply.github.com>
2025-02-13 02:56:02 +0000

    update

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,16 +1,13 @@
 pub fn dp_rec_mc(amount: u32) -> u32 {
     // TODO: 这里写逻辑
     let mut res = 0;
-    let mut cur = amount.clone();
+    let mut cur = amount;
     let mut currencies = vec![1, 2, 5, 10, 20, 30, 50, 100];
     currencies.reverse();
     for kind in currencies.into_iter() {
-        if cur <= 0 {
-            break;
-        }
-        if cur >= kind {
-            res += cur / kind;
-            cur -= (cur / kind) * kind;
+        while cur >= kind {
+            cur -= kind;
+            res += 1;
         }
     }
     res
